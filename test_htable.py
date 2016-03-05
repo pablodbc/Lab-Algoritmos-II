b90190a0cf9b93611bb424dd3810b3152eec2ed8
Pablo Betancourt <pablodbc30@mail.com>
2016-03-05 11:41:49 -0430

    Cliente en proceso

--- test_htable.py
+++ test_htable.py
@@ -1,10 +1,23 @@
 from hashEntry import *
 from hash_table import *
 from dlist import *
-from random import*
+from random import *
+from string import *
 
-tabla = hashTable(20)
-for i in range(20):
-	tabla.insert(randint(0,100),"holaJose")
-print
-print tabla
+def checkSize(valorEsperado, valorReal):
+	if valorReal == valorEsperado:
+		return True
+	else:
+		return False
+def randomstr(N):
+	return ''.join(choice(lowercase) for i in range(N))
+def checkNodeInsertion(tabla):
+	key = randint(0,tabla.size-1)
+	data = randomstr(key+1)
+	node  = hashEntry(key,data)
+	pos = f(key)
+	tabla.slot[pos]
+	tabla.insertElem(node)
+
+
+
